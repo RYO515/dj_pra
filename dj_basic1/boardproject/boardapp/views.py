707b211848fb48ee0dcabff3b46f00d42fc32b72
RYO515 <ryota.miyake.515@gmail.com>
2021-03-21 21:56:32 +0900

    created CreateView

--- dj_basic1/boardproject/boardapp/views.py
+++ dj_basic1/boardproject/boardapp/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from .models import BoardModel
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -60,3 +62,9 @@ def readfunc(request, pk):
         object.readtext = object.readtext + ' ' + username
         object.save()
         return redirect('list')
+
+class BoardCreate(CreateView):
+    template_name = 'create.html'
+    model = BoardModel
+    fields = ('title', 'content', 'author', 'snsimage')
+    success_url = reverse_lazy('list')
